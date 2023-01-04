27215fceaf09d52ea509696e5fef98a957e57981
Meecore4 <[alicia.critelli4@gmail.com](mailto:alicia.critelli4@gmail.com)>
2023-01-04 15:38:51 +0100

    final

---
+++ add.ts
@@ -0,0 +1,28 @@
+class Person{
+    name : string;
+    age : number;
+
+constructor(name : string, age : number){
+    this.name = name;
+    this.age = age;
+
+}
+
+tellMyName(){
+    console.log(`I am ${this.name}`)
+}
+
+tellMyAge(){
+    console.log(`I am ${this.age} years old`)
+}
+
+}
+
+const person1 = new Person('John', 40);
+const person2 = new Person('Mary', 35);
+
+person1.tellMyName();
+person1.tellMyAge();
+
+person2.tellMyName();
+person2.tellMyAge();
